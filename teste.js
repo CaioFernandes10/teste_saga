3641bd77ef3c6c9cd126a900933c36b8126dac1f
caio Fernandes <caiofernandes@pop-os.localdomain>
2023-03-08 11:05:11 -0300

    modolo bolsa do saga

--- teste.js
+++ teste.js
@@ -1,47 +1,177 @@
-const {Builder,By,Key} = require('selenium-webdriver');
-
-// const chrome = require('selenium-webdriver/chrome');
+const { Builder, By, Key, Select, assert, ok } = require('selenium-webdriver');
 const chromedriver = require('chromedriver');
 
-     let chrome = require('selenium-webdriver/chrome');
- 
-      let service = new chrome.ServiceBuilder()
-          .loggingTo('/my/log/file.txt')
-          .enableVerboseLogging()
-          .build();
-    
-      let options = new chrome.Options();
-      // configure browser options ...  
- 
-      let driver = chrome.Driver.createSession(options, service);
- 
+let chrome = require('selenium-webdriver/chrome');
 
-async function run(){
+let service = new chrome.ServiceBuilder()
+    .loggingTo('/my/log/file.txt')
+    .enableVerboseLogging()
+    .build();
 
-    let driver = await new Builder().forBrowser('chrome').build();
-    await driver.get('http://sagapoc.unisuam.edu.br/saga/nucleo/login.php');
-    const searchBar = await driver.findElement(By.name('login'));
-    const senha = await driver.findElement(By.name('senha'));
-    await searchBar.sendKeys('1002562');
-    await senha.sendKeys('123456');
-    await senha.sendKeys(Key.ENTER);
+let options = new chrome.Options();
+var fs = require('fs');
+let driver = chrome.Driver.createSession(options, service);
 
-     const gra = await driver.findElement(By.xpath('/html/body/table/tbody/tr[1]/td/table[2]/tbody/tr[1]/td/table/tbody/tr/td[13]/a'));
-     await gra.click();
 
-     const registro_aluno = await driver.findElement(By.xpath('/html/body/table/tbody/tr[2]/td/form/table/tbody/tr/td[1]/ul/li[6]/div'));
-     await  registro_aluno.click();
-     
-     const matricula_aluno = await driver.findElement(By.xpath('/html/body/table/tbody/tr[2]/td/form/table/tbody/tr/td[1]/ul/li[6]/ul/li[7]/a'));
-     await matricula_aluno.click();
 
-     const  input_matricula = await driver.findElement(By.name('PesquisaValorText'));
-     await  input_matricula.sendKeys('18102647');
-     await  input_matricula.sendKeys(Key.ENTER);
+async function run() {
+
+    //formato da data atual, criar nome do arquivo csv
+    var dataAtual = new Date();
+    var dia = dataAtual.getDate().toString().padStart(2, '0');
+    var mes = (dataAtual.getMonth() + 1).toString().padStart(2, '0');
+    var ano = dataAtual.getFullYear().toString();
+    var hora = dataAtual.getHours().toString().padStart(2, '0');
+    var minutos = dataAtual.getMinutes().toString().padStart(2, '0');
+    var dataFormatada = ano + '_' + mes + '_' + dia + '_' + hora + '_' + minutos;
+    var nome = 'relatorio_sagapoc' + dataFormatada + '.csv';
+    //variaveis de tratamento de erro
+    var log_funcionalidade = '';
+    var log_resultado = '';
+    var log_erro = 'erro';
+
+    // function verifcar_url(url){
+    //     if(url.tpSting().includes('sagapoc')){
+    //         return true;
+    //     }
+    //     return false;
+
+    // }
+
         
 
-     const  processos = await driver.findElement(By.id('bt_man123'));
-     await  processos.click();
-}   
+    var errors = [];
+
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    await driver.get('http://sagapoc.unisuam.edu.br/saga/nucleo/login.php');
+
+    // if(!verifcar_url(driver.getCurrentUrl)){
+    //  throw 'url invalida, nao e o sagapoc';   
+    // }
+    try {
+        //tela de login   
+        const searchBar = await driver.findElement(By.name('login'));
+        const senha = await driver.findElement(By.name('senha'));
+        await searchBar.sendKeys('1002562');
+        await senha.sendKeys('123456');
+        await senha.sendKeys(Key.ENTER);
+        log_funcionalidade = 'tela de login';
+        log_resultado = 'Passou';
+        log_erro = ''
+    } catch (error) {
+        log_funcionalidade = 'tela de login';
+        log_resultado = 'Nao Passou';
+        log_erro = 'Erro ao acessar a tela de login';
+    }
+    fs.appendFile(nome, log_resultado + ';' + log_funcionalidade + ';' + log_erro + ';\n', function (err) {
+        if (err) throw err;
+
+    });
+
+    try {
+        //tela de graduacao    
+        const gra = await driver.findElement(By.xpath('/html/body/table/tbody/tr[1]/td/table[2]/tbody/tr[1]/td/table/tbody/tr/td[13]/a'))
+        await gra.click();
+        log_funcionalidade = 'tela de graduacao';
+        log_resultado = 'Passou';
+        log_erro = ''
+    } catch (error) {
+        log_funcionalidade = 'tela de graduacao';
+        log_resultado = 'Nao Passou';
+        log_erro = 'Erro ao clikar no botao de Graduacao';
+    }
+    fs.appendFile(nome, log_resultado + ';' + log_funcionalidade + ';' + log_erro + ';\n', function (err) {
+        if (err) throw err;
+    });
+
+
+    try {
+        //bolsa>cadastro>cadastro de motivos
+        const bolsa = await driver.findElement(By.xpath('/html/body/table/tbody/tr[2]/td/form/table/tbody/tr/td[1]/ul/li[1]/div'));
+        await bolsa.click();
+
+        const cadastro = await driver.findElement(By.xpath('/html/body/table/tbody/tr[2]/td/form/table/tbody/tr/td[1]/ul/li[1]/ul/li[1]/div'));
+        await cadastro.click();
+
+        const cadastro_motivos = await driver.findElement(By.xpath('/html/body/table/tbody/tr[2]/td/form/table/tbody/tr/td[1]/ul/li[1]/ul/li[1]/ul/li[1]/a/div'));
+        await cadastro_motivos.click();
+        log_funcionalidade = 'Cadastro de motivos para bolsa';
+        log_resultado = 'Passou';
+        log_erro = ''
+
+    } catch (error) {
+        log_funcionalidade = 'Cadastro de motivos para bolsa';
+        log_resultado = 'Nao Passou';
+        log_erro = 'Erro ao entrar na tela de Cadastro de motivos para bolsa';
+    }
+    fs.appendFile(nome, log_resultado + ';' + log_funcionalidade + ';' + log_erro + ';\n', function (err) {
+        if (err) throw err;
+    });
+    try {
+        //inclusao de cadastro de bolsa
+        const incluir = await driver.findElement(By.xpath('//*[@id="T_principal"]/center/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr/td[2]'));
+        await incluir.click();
+
+        const status = await driver.findElement(By.name('frm_statuspedidos'));
+        await status.click();
+
+        let select = new Select(status);
+        await select.selectByIndex(1);
+
+        const desc = await driver.findElement(By.name('frm_descricao'));
+        await desc.sendKeys('teste');
+        await desc.sendKeys(Key.ENTER);
+        log_funcionalidade = 'Inclusão de motivos de bolsa';
+        log_resultado = 'Passou';
+        log_erro = ''
+
+    } catch (error) {
+        log_funcionalidade = 'Inclusão de motivos de bolsa';
+        log_resultado = 'Nao Passou';
+        log_erro = 'Erro na tela de Inclusão de motivos de bolsa';
+    }
+    fs.appendFile(nome, log_resultado + ';' + log_funcionalidade + ';' + log_erro + ';\n', function (err) {
+        if (err) throw err;
+    });
+
+    //confirma o cadastro
+    const confirmacao = await driver.findElement(By.name('ok'));
+    await confirmacao.click();
+
+    try {
+        //pesquisar elemento criado na tela de cadastro de bolsa
+        const pesquisa = await driver.findElement(By.name('pesquisa'));
+        await pesquisa.sendKeys('teste');
+        await pesquisa.sendKeys(Key.ENTER);
+        log_funcionalidade = 'Pesquisa do elemento criado';
+        log_resultado = 'Passou';
+        log_erro = ''
+    } catch (error) {
+        log_funcionalidade = 'Pesquisa do elemento criado(Cadastro de motivos para bolsa)';
+        log_resultado = 'Nao Passou';
+        log_erro = 'Erro na pesquisa';
+
+    }
+    fs.appendFile(nome, log_resultado + ';' + log_funcionalidade + ';' + log_erro + ';\n', function (err) {
+        if (err) throw err;
+    });
+
+    setTimeout(async () => {
+  
+        await driver.quit();
+        },
+        30000);
+
+
+
+
+
+
+
+
+
+
+}
 
 run();
